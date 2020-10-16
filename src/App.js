56fdfbbeb305cbcfd365b38d94a5d631d39c91b9
susuuBin <azxq1000@gmail.com>
2020-10-16 12:51:57 +0900

    #2.2 Dynamic Component Generaion 동영상 실습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+function Book({name, image}) {
+  return (
+    <div>
+      <h2> {name} </h2>
+      <img src={image} />
+    </div>
+  );
+}
+
+const BookList = [
+  {
+    name: "전천당 7",
+    image:
+      "https://gfile.gilbut.co.kr//gilbut/dang_samplebook/202008//book_07.jpg"
+  },
+  {
+    name: "고무줄 늘리기",
+    image:
+      "https://cdn.news2day.co.kr/data/tmp/2003/20200313150047_ydkqtwba.jpg"
+  },
+  {
+    name: "엉덩이 탐정 6",
+    image:
+      "https://image.yes24.com/goods/60564391/800x0"
+  },
+  {
+    name: "설민석의 한국사 대모험 15",
+    image:
+      "https://image.yes24.com/momo/TopCate3060/MidCate010/305189451.jpg"
+  }
+]
+
+// 이 application에서 Food Component로 정보를 보내고 그런 다음에 Food Component에서 그 정보를 어떻게 사용하는지
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { BookList.map( popular => (
+        <Book name={popular.name} image={popular.image} />
+      ))}
     </div>
   );
 }
