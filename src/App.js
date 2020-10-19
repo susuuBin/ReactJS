ce98b3fedf2b19f63789d3c84efe4c5db8b00135
susuuBin <azxq1000@gmail.com>
2020-10-19 17:36:26 +0900

    #3.2 Component Life Cycle 동영상 실습

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Book({name, image}) {
-  return (
-    <div>
-      <h2> {name} </h2>
-      <img src={image} />
-    </div>
-  );
-}
+class App extends React.Component {
 
-const BookList = [
-  {
-    name: "전천당 7",
-    image:
-      "https://gfile.gilbut.co.kr//gilbut/dang_samplebook/202008//book_07.jpg"
-  },
-  {
-    name: "고무줄 늘리기",
-    image:
-      "https://cdn.news2day.co.kr/data/tmp/2003/20200313150047_ydkqtwba.jpg"
-  },
-  {
-    name: "엉덩이 탐정 6",
-    image:
-      "https://image.yes24.com/goods/60564391/800x0"
-  },
-  {
-    name: "설민석의 한국사 대모험 15",
-    image:
-      "https://image.yes24.com/momo/TopCate3060/MidCate010/305189451.jpg"
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  minus = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  componentDidMount(){
+    console.log("componet rednered");
   }
-]
 
-// 이 application에서 Food Component로 정보를 보내고 그런 다음에 Food Component에서 그 정보를 어떻게 사용하는지
-function App() {
-  return (
-    <div>
-      { BookList.map( popular => (
-        <Book name={popular.name} image={popular.image} />
-      ))}
-    </div>
-  );
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  
+  render(){
+    console.log("I'm rendering");
+    return (
+               
+      <div> 
+        <h1> The number is {this.state.count} </h1>
+        <button onClick={this.add}> Add </button>
+        <button onClick={this.minus}> Minus </button>
+      </div>
+    );
+  }
 }
 
 export default App;
